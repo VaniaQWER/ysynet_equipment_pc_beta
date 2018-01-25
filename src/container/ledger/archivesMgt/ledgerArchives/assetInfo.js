bd20892ae274cd24de8f00549e1cd1d7609cc56b
sarah0730 <943349411@qq.com>
2018-01-25 10:28:02 +0800

    资产信息编辑
    update

--- src/container/ledger/archivesMgt/ledgerArchives/assetInfo.js
+++ src/container/ledger/archivesMgt/ledgerArchives/assetInfo.js
@@ -2,16 +2,34 @@
  * 档案管理-资产档案-详情-基本信息-资产信息
  */
 import React, { Component } from 'react';
-import { Row, Col } from 'antd';
+import { Row, Col,message } from 'antd';
 import InputWrapper from '../../../../component/inputWrapper'
 import styles from './style.css';
+import { withRouter } from 'react-router'
+import { connect } from 'react-redux';
+import { ledger as ledgerService } from '../../../../service';
+import assets from '../../../../api/assets';
 
 
 class AssetInfo extends Component {
-  render () {
-    console.log(this.props)
-    const { AssetInfoData } = this.props;
+  handleUpdateAssetsRecordInfo = (data,field) =>{
+    const { updateAssetsRecordInfo,AssetInfoData } = this.props;
+    let params = { };
+    params[field] = data;
+    params.assetsRecordGuid = AssetInfoData.assetsRecordGuid;
+   updateAssetsRecordInfo(assets.updateAssetsRecordInfo,params,(data) => {
+     if(data.status){
+      message.success("修改成功")
+     }else{
+      message.error(data.msg);
+     }
+   })
+  }
 
+
+
+  render () {
+    const { AssetInfoData } = this.props;
     return (
       <Row type="flex" style={{marginTop: 16}}  className={styles['table-row']}>
         <Col span={4} className={styles['table-span']}>资产名称</Col>
@@ -19,7 +37,7 @@ class AssetInfo extends Component {
         <Col span={4} className={styles['table-span']}>资产编号</Col>
         <Col span={8} className={styles['table-span']}>{ AssetInfoData.assetsRecord }</Col>
         <Col span={4} className={styles['table-span']}>通用名称</Col>
-        <Col span={8} className={styles['table-span']}><InputWrapper text={ AssetInfoData.productType } /></Col>
+        <Col span={8} className={styles['table-span']}><InputWrapper onEndEdit={(data) => this.handleUpdateAssetsRecordInfo(data,'productType')} text={ AssetInfoData.equipmetStandardName } /></Col>
         <Col span={4} className={styles['table-span']}>状态</Col>
         <Col span={8} className={styles['table-span']}>{ AssetInfoData.useFstate }</Col>
         <Col span={4} className={styles['table-span']}>型号</Col>
@@ -33,7 +51,7 @@ class AssetInfo extends Component {
         <Col span={4} className={styles['table-span']}>保管员</Col>
         <Col span={8} className={styles['table-span']}>{ AssetInfoData.custodian }</Col>
         <Col span={4} className={styles['table-span']}>存放地址</Col>
-        <Col span={8} className={styles['table-span']}><InputWrapper text={ AssetInfoData.deposit } /></Col>
+        <Col span={8} className={styles['table-span']}><InputWrapper text={ AssetInfoData.deposit} onEndEdit={(data) => this.handleUpdateAssetsRecordInfo(data,'DEPOSIT')}/></Col>
         <Col span={4} className={styles['table-span']}>管理科室</Col>
         <Col span={8} className={styles['table-span']}>{ AssetInfoData.bDept }</Col>
         <Col span={4} className={styles['table-span']}>注册证号</Col>
@@ -61,13 +79,13 @@ class AssetInfo extends Component {
         <Col span={4} className={styles['table-span']}>经费来源</Col>
         <Col span={8} className={styles['table-span']}>{ AssetInfoData.sourceFunds }</Col>
         <Col span={4} className={styles['table-span']}>维修分类</Col>
-        <Col span={8} className={styles['table-span']}><InputWrapper text={ AssetInfoData.rrpairType } /></Col>
+        <Col span={8} className={styles['table-span']}><InputWrapper text={ AssetInfoData.rrpairType } onEndEdit={(data) => this.handleUpdateAssetsRecordInfo(data,'RRPAIR_TYPE')} /></Col>
         <Col span={4} className={styles['table-span']}>保养分类</Col>
-        <Col span={8} className={styles['table-span']}><InputWrapper text={ AssetInfoData.maintainType } /></Col>
+        <Col span={8} className={styles['table-span']}><InputWrapper text={ AssetInfoData.maintainType } onEndEdit={(data) => this.handleUpdateAssetsRecordInfo(data,'MAINTAIN_TYPE')} /></Col>
         <Col span={4} className={styles['table-span']}>计量分类</Col>
-        <Col span={8} className={styles['table-span']}><InputWrapper text={ AssetInfoData.meteringType } /></Col>
+        <Col span={8} className={styles['table-span']}><InputWrapper text={ AssetInfoData.meteringType } onEndEdit={(data) => this.handleUpdateAssetsRecordInfo(data,'METERING_TYPE')} /></Col>
         <Col span={4} className={styles['table-span']}>保养周期</Col>
-        <Col span={8} className={styles['table-span']}><InputWrapper text={ AssetInfoData.maintainDay } /></Col>
+        <Col span={8} className={styles['table-span']}><InputWrapper text={ AssetInfoData.maintainDay } onEndEdit={(data) => this.handleUpdateAssetsRecordInfo(data,'MAINTAIN_DAY')} /></Col>
         <Col span={4} className={styles['table-span']}>下次保养</Col>
         <Col span={8} className={styles['table-span']}>{ AssetInfoData.nextMaintainDate }</Col>
         <Col span={4} className={styles['table-span']}>保修截止期</Col>
@@ -85,12 +103,15 @@ class AssetInfo extends Component {
         <Col span={4} className={styles['table-span']}>预计使用年限</Col>
         <Col span={8} className={styles['table-span']}>{ AssetInfoData.useLimit }</Col>
         <Col span={4} className={styles['table-span']}>有无备用</Col>
-        <Col span={8} className={styles['table-span']}><InputWrapper text={ AssetInfoData.spare } /></Col>
+        <Col span={8} className={styles['table-span']}><InputWrapper text={ AssetInfoData.spare } onEndEdit={(data) => this.handleUpdateAssetsRecordInfo(data,'SPARE')} /></Col>
       </Row>
     )
   }
 }
 
- export default AssetInfo;
+
+ export default withRouter(connect(null, dispatch => ({
+  updateAssetsRecordInfo: (url,values,success) => ledgerService.getInfo(url,values,success),
+}))(AssetInfo));
 
 
