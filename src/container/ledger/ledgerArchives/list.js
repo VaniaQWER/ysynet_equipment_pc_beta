c299b2e23cf7968cc7ccee71b5b1f389e41cbf06
yuwei <178967037@qq.com>
2018-06-21 16:49:35 +0800

    档案管理-资产档案

--- src/container/ledger/ledgerArchives/list.js
+++ src/container/ledger/ledgerArchives/list.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-import { Row,Col,Input,Icon, Layout,Upload,Button,Tag,message,Alert } from 'antd';
+import { Row,Col,Input,Icon, Layout,Upload,Button,Tag,message,Menu,Dropdown,Alert, Form,Select} from 'antd';
 import TableGrid from '../../../component/tableGrid';
 import { Link } from 'react-router-dom'
 import assets from '../../../api/assets';
 import styles from './style.css';
-import { ledgerData,productTypeData } from '../../../constants';
-import querystring from 'querystring';
+import { ledgerData,productTypeData,useFstateSel } from '../../../constants';
+import request from '../../../utils/request';
+import queryString from 'querystring';
+
 const { Content } = Layout;
-const Search = Input.Search;
+const FormItem = Form.Item;
+const Option = Select.Option;
 const { RemoteTable } = TableGrid;
 const columns = [
   {
@@ -17,7 +20,7 @@ const columns = [
     render: (text, record) => 
       <span>
         <Link to={{pathname: `/ledger/ledgerArchives/${record.assetsRecordGuid}`}}><Icon type="profile" />详情</Link>
-        <a className={styles['ml10']} target="_blank" href={assets.printEquipmentQrcode+"?"+querystring.stringify({assetsRecordGuid:record.assetsRecordGuid})}>打码</a> 
+        <a className={styles['ml10']} target="_blank" href={assets.printEquipmentQrcode+"?"+queryString.stringify({assetsRecordGuid:record.assetsRecordGuid})}>打码</a> 
         <Link className={styles['ml10']}  to={{pathname: `/ledger/benefitAnalysis/${record.assetsRecordGuid}`}}><Icon type="profile" />效益分析</Link>
       </span>  
   },
@@ -31,7 +34,9 @@ const columns = [
     title: '状态',
     dataIndex: 'useFstate',
     width: 100,
-     render: text =>  <Tag color={ledgerData[text].color}> { ledgerData[text].text } </Tag>
+    filters: useFstateSel,
+    onFilter: (value, record) => (record && record.useFstate===value),
+    render: text =><Tag color={ledgerData[text].color}> { ledgerData[text].text } </Tag>,
   },
   {
     title: '资产名称',
@@ -40,7 +45,7 @@ const columns = [
   },
   {
     title: '型号',
-    dataIndex: 'spec',
+    dataIndex: 'fmodel',
     width: 100
   },
   {
@@ -66,20 +71,245 @@ const columns = [
   }
 ];
 const messageInfo = "添加大量的信息，建议使用导入功能。导入前请先下载Excel格式模版文件。";
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 8 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 16 },
+  },
+};
+
+class SearchForm extends Component {
+
+  state={
+    display: 'none',
+    manageSelect:[],
+    outDeptOptions: []
+  }
+  componentDidMount = () => {
+    this.getManageSelect();
+    this.outDeptSelect();
+  }
+
+  getManageSelect = () => {
+    request(assets.selectUseDeptList,{
+      body:queryString.stringify({deptType:"01"}),
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      success: data => {
+        if(data.status){
+                this.setState({manageSelect:data.result})
+        }else{
+                message.error(data.msg)
+        }
+      },
+      error: err => {console.log(err)}
+    })
+  }
+  outDeptSelect = () => {
+    request(assets.selectUseDeptList,{
+      body:queryString.stringify({deptType:"00"}),
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      success: data => {
+        if(data.status){
+                this.setState({outDeptOptions:data.result})
+        }else{
+                message.error(data.msg)
+        }
+      },
+      error: err => {console.log(err)}
+    })
+  }
+
+  toggle = () => {
+    const { display, expand } = this.state;
+    this.setState({
+      display: display === 'none' ? 'block' : 'none',
+      expand: !expand
+    })
+  }
+
+  handleSearch = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      console.log(values)
+      this.props.query(values);
+    });
+  }
+  handleReset = () => {
+    this.props.form.resetFields();
+    this.props.query({});
+  }
+  render(){
+    const { getFieldDecorator } = this.props.form;
+    const { display } = this.state;
+    return (
+      <Form  onSubmit={this.handleSearch}>
+        <Row>
+          <Col span={6}> 
+            <FormItem
+              {...formItemLayout}
+              label="资产编码"
+            >
+              {getFieldDecorator('assetCode')(
+                <Input placeholder='请输入资产编码'/>
+              )}
+            </FormItem>
+          </Col>
+          <Col span={6}> 
+            <FormItem
+              {...formItemLayout}
+              label="资产名称"
+            >
+              {getFieldDecorator('assetName')(
+                <Input placeholder='请输入资产名称'/>
+              )}
+            </FormItem>
+          </Col>
+          <Col span={6}> 
+            <FormItem
+              {...formItemLayout}
+              label="型号"
+            >
+              {getFieldDecorator('fmodel')(
+                <Input  placeholder='请输入型号'/>
+              )}
+            </FormItem>
+          </Col>
+          <Col span={6} style={{textAlign:'right',paddingRight:15,paddingTop:5}}> 
+              <Button type="primary" htmlType="submit">搜索</Button>
+              <Button style={{marginLeft: 30,}} onClick={this.handleReset}>重置</Button>
+              <a style={{marginLeft: 30, fontSize: 14}} onClick={this.toggle}>
+                {this.state.expand ? '收起' : '展开'} <Icon type={this.state.expand ? 'up' : 'down'} />
+              </a>
+          </Col>
+        </Row>
+        <Row style={{display: display}}>
+          <Col span={6}> 
+            <FormItem
+              {...formItemLayout}
+              label="规格"
+            >
+              {getFieldDecorator('spec')(
+                <Input  placeholder='请输入规格'/>
+              )}
+            </FormItem>
+          </Col>
+          <Col span={6}> 
+            <FormItem
+              {...formItemLayout}
+              label="管理科室"
+            >
+              {getFieldDecorator('manageDeptGuid',{
+                initialValue:""
+              })(
+                <Select 
+                showSearch
+                placeholder={'请选择'}
+                optionFilterProp="children"
+                filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+                >
+                    <Option value="" key={-1}>全部</Option>
+                    {
+                        this.state.manageSelect.map((item,index) => {
+                        return <Option key={index} value={item.value}>{item.text}</Option>
+                        })
+                    }
+                </Select>
+              )}
+            </FormItem>
+          </Col>
+          <Col span={6}> 
+            <FormItem
+              {...formItemLayout}
+              label="使用科室"
+            >
+              {getFieldDecorator('useDeptGuid',{
+                initialValue:""
+              })(
+                <Select 
+                showSearch
+                placeholder={'请选择'}
+                optionFilterProp="children"
+                filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+                >
+                    <Option value="" key={-1}>全部</Option>
+                    {
+                        this.state.outDeptOptions.map((item,index) => {
+                        return <Option key={index} value={item.value.toString()}>{item.text}</Option>
+                        })
+                    }
+                </Select>
+              )}
+            </FormItem>
+          </Col>
+        </Row>
+        <Row style={{display: display}}>
+          <Col span={6}> 
+            <FormItem
+              {...formItemLayout}
+              label="保管员"
+            >
+              {getFieldDecorator('custodian')(
+                <Input placeholder='请输入保管员'/>
+              )}
+            </FormItem>
+          </Col>
+        </Row>
+        
+      </Form>
+    )
+  }
+}
+const SearchFormWapper = Form.create()(SearchForm);
+
 class LedgerArchivesList extends Component {
   constructor(props) {
     super(props);
     this.state = {
       loading: false,
-      query:{},
-      messageError:""
+      query:{"deptType":"MANAGEMENT"},
+      messageError:"",
+      selectedRowKeys:[]
     }
   }
   queryHandler = (query) => {
-    this.refs.table.fetch(query);
-    this.setState({ query })
+    let q = Object.assign({"deptType":"MANAGEMENT"},query);
+    this.refs.table.fetch(q);
+    this.setState({ q })
+  }
+  query = (val) => {
+    this.refs.table.fetch(val)
+  }
+  //打印所有
+  printAll = () =>{
+    let v = this.refs.form.getFieldsValue();
+    this.sendPrintAjax(v)
+  }
+  //选择资产打印
+  printSelect = ()=>{
+    //查看选择内容
+    const {selectedRowKeys} = this.state;
+    if(selectedRowKeys.length>0){
+      let json = {assetsGuids:selectedRowKeys}
+      this.sendPrintAjax(json)
+    }else{
+      message.warn('请选择相关资产信息打印')
+    }
+  }
+
+  sendPrintAjax = (json) => {
+    console.log('发出的数据内容',JSON.stringify(json))
+    window.open(assets.printEquipmentQrcode+"?"+queryString.stringify(json))
   }
   render() {
+    const { selectedRowKeys } = this.state;
     return (
       <Content className='ysynet-content ysynet-common-bgColor'>
          <Alert message={messageInfo} type="warning" showIcon closeText="关闭" />
@@ -88,15 +318,9 @@ class LedgerArchivesList extends Component {
             :
             <Alert style={{marginTop : 10}} message="错误提示"  type="error" description={<div dangerouslySetInnerHTML={{__html:this.state.messageError}}></div>} showIcon closeText="关闭" />
           }
+          <SearchFormWapper query={query=>{this.query(query)}} ref='form'/>
           <Row style={{marginTop : 10}}>
-            <Col span={12}>
-              <Search
-                placeholder="请输入资产编号/资产名称"
-                onSearch={value =>  {this.queryHandler({'mobile':value})}}
-                style={{ width: 400 }}
-                enterButton="搜索"
-              />
-            </Col>
+            <Col span={12}></Col>
             <Col span={8} className={styles['text-align-right']}>
               <Upload
                 action={assets.importEquipments}
@@ -127,10 +351,21 @@ class LedgerArchivesList extends Component {
                 <Button style={{ marginRight: 8 }}>
                   <Icon type='export'/> 导入
                 </Button>
-                <a href={assets.importAssetsTemplate} style={{ marginLeft: 8 }} target="_self">
+                
+                
+                {/*<a href={assets.importAssetsTemplate} style={{ marginLeft: 8 }} target="_self">
                   <Icon type='cloud-download'/> 下载模板
-                </a>
+                </a>*/}
               </Upload>
+              <Dropdown overlay={
+                <Menu  onClick={()=>this.printAll()}>
+                  <Menu.Item key="1"> 批量打印</Menu.Item>
+                </Menu>
+              } >
+                <Button onClick={()=>this.printSelect()}>
+                  打印 <Icon type="ellipsis" />
+                </Button>
+              </Dropdown>
             </Col>
             <Col span={4} style={{textAlign:'right'}}>
               <Button type='primary' style={{ marginRight: 16 }}>
@@ -149,6 +384,12 @@ class LedgerArchivesList extends Component {
             rowKey={'assetsRecordGuid'}
             style={{marginTop: 10}}
             size="small"
+            rowSelection={{
+              selectedRowKeys,
+              onChange: (selectedRowKeys) => {
+                this.setState({selectedRowKeys})
+              }
+            }}
           /> 
         </Content>
     )
