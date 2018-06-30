e7c2ffb35b457eda8098a0ef8e122935e633cdab
yuwei <178967037@qq.com>
2018-06-30 15:14:00 +0800

    【新增】-资产借用路由配置

--- src/route/index.js
+++ src/route/index.js
@@ -19,13 +19,15 @@ import metermgt from './metermgt';
 import deptwork from './deptwork';
 import storage from './storage';
 import benefitAnalysis from './benefitAnalysis';
+import ledgerBorrow from './ledgerBorrow';
 const routes = [
   { path: '/login', exact: true, component: Login },
   { path: '/register', exact: true, component: () =>  <div>register</div> },
   { path: '/', component: Home, routes: [
     system, ledger, operation, dashboard , upkeep ,basicdata, transfer , inventory, scrap ,devalue , metermgt , deptwork ,
     storage,
-    benefitAnalysis
+    benefitAnalysis,
+    ledgerBorrow
   ]}
 ]
 
